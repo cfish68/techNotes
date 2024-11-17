d58c735bb054cebe058e2d2d184b005fbec7e8e2
csfwn <csfwny@gmail.com>
2024-11-17 20:13:51 +0200

    first stage frontend

---
+++ frontend/src/components/DashLayout.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+import DashHeader from './DashHeader'
+import DashFooter from './Dashfooter'
+const DashLayout = () => {
+  return (
+    <>
+        <DashHeader />
+        <div className='dash-container'>
+            <Outlet />
+        </div>
+        <DashFooter />
+    </>
+  )
+}
+
+export default DashLayout
