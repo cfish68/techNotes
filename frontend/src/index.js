08c81639186765254faec283e8e955177c884188
csfwn <csfwny@gmail.com>
2024-11-18 14:58:23 +0200

    users and notes views implemented

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,17 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { store } from './app/store'
+import { Provider } from 'react-redux'
 
 import { browserRouter, Routes, Route, BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Provider store={store}>
     <BrowserRouter>
       <Routes>
         <Route path="/*" element={<App />} />
       </Routes>
     </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
 
